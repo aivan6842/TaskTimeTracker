845d86992ff1fa32176e271b75d474babdf36471
Alex Laptop <alexivanov15@icloud.com>
2020-06-30 23:09:32 -0400

    Testing Camera

--- Task/urls.py
+++ Task/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, re_path
 
 from . import views
+from django.http import StreamingHttpResponse
+from .Camera import VideoCamera, gen
 
 urlpatterns = [
     path('signIn/', views.signIn),
     path('signUp/', views.signUp),
     path('viewTasks/<id>/', views.viewTasks),
     path('createTask/<id>/', views.createTask),
+    path('cams/', lambda r: StreamingHttpResponse(gen(VideoCamera()),
+                                                     content_type='multipart/x-mixed-replace; boundary=frame')),
     path('', views.home),
 ]
